ff025d6becf51103fc97443bd834155d923fdad4
Yui Yukihira <littlespec200000@gmail.com>
2017-10-06 17:13:44 +0100

    Complete Rewrite of the bot, responses are now customizable and commands are stored in cogs.

---
+++ cogs/server_stuff.py
@@ -0,0 +1,45 @@
+
+import discord, asyncio, subprocess, os, json
+from discord.ext import commands
+
+class Server:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = self.bot.config
+        self.responses = self.config['responses']['server_stuff']
+
+    def server_running():
+        with open(os.devnull, 'wb') as hide_output:
+            exit_code = subprocess.Popen(['service', 'minecraft-server', 'status'], stdout=hide_output, stderr=hide_output).wait()
+            return exit_code == 0
+
+    @commands.command()
+    async def state(self, ctx):
+        """I'll tell you if Server-chan is awake."""
+        if self.server_running():
+            await ctx.send(self.responses['state_on'])
+        else:
+            await ctx.send(self.responses['state_off'])
+
+    @commands.command()
+    async def start(self, ctx):
+        """I'll wake server-chan up if she's asleep"""
+        if self.server_running():
+            await ctx.send(self.responses['start_error_on'])
+        else:
+            await ctx.send(self.responses['start_begining'])
+            p = subprocess.Popen(['service', 'minecraft-server', 'start'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            p.stdin.close()
+            await ctx.send(self.responses['start_end'])
+
+    @commands.command()
+    async def stop(self, ctx):
+        """I'll take Server-chan to bed if she's awake"""
+        if self.server_running():
+            await ctx.send(self.responses['stop_start'])
+            p = subprocess.Popen(['service', 'minecraft-server', 'stop'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            p.stdin.close()
+            await ctx.send(self.responses['stop_end'])
+
+def setup(bot):
+    bot.add_cog(Server(bot))
---
+++ main.py
@@ -0,0 +1,99 @@
+import discord
+from discord.ext import commands
+import asyncio
+import subprocess, os
+import json
+import logging
+import traceback
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(filename='Bot-tan.log', encoding='utf-8', backupCount=1, maxBytes=1024 * 1024 * 50)
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+def embed_split(text: str) -> [str]:
+    """split text to fit embed"""
+    while text:
+        yield text[:1024]
+        text = text[1024:]
+
+class Bot_tan(commands.AutoShardedBot):
+    def __init__(self, config):
+        self.config = json.loads(open(config).read())
+
+        super().__init__(
+                command_prefix='!~',
+                description=self.config['description'],
+                max_messages = 500
+                )
+        self.key = open(self.config['key']).read().strip()
+        self.responses = self.config['responses']['main']
+
+    async def log(self, *args, **kwargs):
+        chn = self.get_channel(self.config['log_channel'])
+        if chn is not None:
+            await chn.send(*args, **kwargs)
+        else:
+            print(*args, **kwargs)
+
+    async def on_command_error(self, ctx, error):
+        if ctx.command is not None:
+            prepared_help = f'{ctx.prefix}{ctx.command.signature}'
+
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send(self.responses['private_message_error'])
+            return
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send(f'Command is missing required argument: {error}\nUsage: {prepared_help}', delete_after=10)
+            return
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(f"{error}\nUsage: `{prepared_help}`", delete_after=10)
+            return
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(error, delete_after=5)
+            return
+        elif isinstance(error, commands.CommandNotFound):
+            return
+
+    async def on_ready(self):
+        for i in self.shards:
+            await self.change_presence(game=discord.Game(name='Taking care of Server-chan'), shard_id = i)
+
+        cogs_s, cogs_f = await self.load_cogs()
+
+        embed = discord.Embed(title='Import modules', colour=0xc67647)
+
+        for i in embed_split(', '.join(cogs_s)):
+            embed.add_field(name="Successful", value=i)
+        for i in embed_split(', '.join(f'{k}: {j}' for k, j in cogs_f)):
+            embed.add_field(name="Failed", value=i)
+        await self.log(embed=embed)
+
+    async def load_cogs(self):
+        for extension in self.extensions.copy():
+            self.unload_extension(entension)
+
+        imported_modules = []
+        failed_modules = []
+
+        for i in self.config['extensions']:
+            try:
+                self.load_extension(i)
+            except ImportError:
+                failed_modules.append([i, traceback.format_exc()])
+            else:
+                imported_modules.append(i)
+
+        return imported_modules, failed_modules
+
+    def run(self):
+        super().run(self.key)
+
+    async def close(self):
+        await super().close()
+
+if __name__ == '__main__':
+    bot = Bot_tan('bot.json')
+    bot.run()
